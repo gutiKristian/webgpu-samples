8681a772b21f1d5917a490a587d79d0fc94d8be8
Kristian Gutic <kristian.gutic@gmail.com>
2023-09-05 22:50:24 +0200

    Simple info about adapter

--- src/scripts/mainPageGpuInfo/main.ts
+++ src/scripts/mainPageGpuInfo/main.ts
@@ -1,4 +1,10 @@
 import { GPUSetup } from "../../GPUSetup";
 
 const gpu = await GPUSetup.build("dummy-canvas");
-gpu.listGPUInfo();
+const info = await gpu.adapter.requestAdapterInfo();
+(<HTMLLIElement> document.getElementById("architecture")).innerHTML = info.architecture;
+(<HTMLLIElement> document.getElementById("description")).innerHTML = info.description;
+(<HTMLLIElement> document.getElementById("device")).innerHTML = info.device;
+(<HTMLLIElement> document.getElementById("vendor")).innerHTML = info.vendor;
+//@ts-ignore missing in GPUAdapterInfo ?
+(<HTMLLIElement> document.getElementById("driver")).innerHTML = info.driver;
